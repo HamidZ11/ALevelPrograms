7ddd83500b0a9496f6f223a456ef5a2dc3ca8e49
Hamid <hamida7106@gmail.com>
2024-12-04 10:48:03 +0000

    Add files via upload

---
+++ Lottery/Lottery/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the maximum number:");
+            int max = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                Random rnd = new Random();
+
+                Console.WriteLine("Random numbers:");
+                for (int i = 0; i < 6; i++)
+                {
+                    int randomNumber = rnd.Next(1, max + 1);
+                    Console.Write(randomNumber + " ");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Would you like another go? (yes/no)");
+                string response = Console.ReadLine();
+
+                if (response.ToLower() == "no")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
