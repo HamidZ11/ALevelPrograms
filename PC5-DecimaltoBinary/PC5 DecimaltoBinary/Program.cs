d251be23b17a2c0c7b43a4b6d55274d6902e7cc3
Hamid <hamida7106@gmail.com>
2024-12-04 10:32:39 +0000

    Add files via upload

---
+++ PC5-DecimaltoBinary/PC5 DecimaltoBinary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a decimal number:");
+        int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+        string binary = DecimalToBinary(decimalNumber);
+        Console.WriteLine("Binary representation: " + binary);
+
+        Console.ReadLine();
+    }
+
+    static string DecimalToBinary(int decimalNumber)
+    {
+        string binary = string.Empty;
+
+        while (decimalNumber > 0)
+        {
+            int remainder = decimalNumber % 2;
+            binary = remainder.ToString() + binary;
+            decimalNumber = decimalNumber / 2;
+        }
+
+        return binary;
+    }
+}
