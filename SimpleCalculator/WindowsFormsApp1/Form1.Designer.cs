f34fe34ae48ee1c88dd749f4022d536bf165eec2
Hamid <hamida7106@gmail.com>
2024-12-04 10:28:19 +0000

    Add files via upload

---
+++ SimpleCalculator/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.v1 = new System.Windows.Forms.TextBox();
+            this.v2 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Value 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Value 2: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 31);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Output: ";
+            // 
+            // v1
+            // 
+            this.v1.Location = new System.Drawing.Point(144, 116);
+            this.v1.Name = "v1";
+            this.v1.Size = new System.Drawing.Size(100, 20);
+            this.v1.TabIndex = 6;
+            this.v1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // v2
+            // 
+            this.v2.Location = new System.Drawing.Point(145, 170);
+            this.v2.Name = "v2";
+            this.v2.Size = new System.Drawing.Size(100, 20);
+            this.v2.TabIndex = 7;
+            this.v2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(144, 225);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(100, 20);
+            this.output.TabIndex = 8;
+            this.output.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(69, 304);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(69, 70);
+            this.add.TabIndex = 9;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtract.Location = new System.Drawing.Point(165, 304);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(69, 70);
+            this.subtract.TabIndex = 10;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(262, 304);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(69, 70);
+            this.divide.TabIndex = 11;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiply.Location = new System.Drawing.Point(354, 304);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(69, 70);
+            this.multiply.TabIndex = 12;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.v2);
+            this.Controls.Add(this.v1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox v1;
+        private System.Windows.Forms.TextBox v2;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button multiply;
+    }
+}
+
---
+++ SimpleCalculator/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(v1.Text);
+            double txt2 = Convert.ToDouble(v2.Text);
+            double additionsum = txt1 + txt2;
+            output.Text = Convert.ToString(additionsum);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(v1.Text);
+            double txt2 = Convert.ToDouble(v2.Text);
+            double additionsum = txt1 - txt2;
+            output.Text = Convert.ToString(additionsum);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(v1.Text);
+            double txt2 = Convert.ToDouble(v2.Text);
+            double additionsum = txt1 / txt2;
+            output.Text = Convert.ToString(additionsum);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(v1.Text);
+            double txt2 = Convert.ToDouble(v2.Text);
+            double additionsum = txt1 * txt2;
+            output.Text = Convert.ToString(additionsum);
+        }
+    }
+}
