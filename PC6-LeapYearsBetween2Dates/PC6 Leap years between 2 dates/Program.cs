f5cffe33758d109bc3bc1495c9fab1416607e11b
Hamid <hamida7106@gmail.com>
2024-12-04 10:46:41 +0000

    Add files via upload

---
+++ PC6-LeapYearsBetween2Dates/PC6 Leap years between 2 dates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the start year:");
+        int startYear = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the end year:");
+        int endYear = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Leap years between " + startYear + " and " + endYear + " are:");
+
+        for (int year = startYear; year <= endYear; year++)
+        {
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine(year);
+            }
+        }
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
