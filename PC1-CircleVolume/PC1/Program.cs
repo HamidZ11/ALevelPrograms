89f26289171af2ce3da5e16003ffbe75b34d5e37
Hamid <hamida7106@gmail.com>
2024-12-04 10:32:11 +0000

    Add files via upload

---
+++ PC1-CircleVolume/PC1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace CircleVolume;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double radius; //declaring the radius as a variable
+        Console.WriteLine("Enter radius: ");
+        radius = Convert.ToDouble(Console.ReadLine()); //saves user input as the radius
+        double volume = (1.33333333) * (Math.PI) * (radius * radius * radius);
+        Console.WriteLine("The volume is " + volume); //output the volume
+        Console.ReadKey();
+    }
+}
